3b5082ec9f428b6c6caf670c765ea6be7744ff20
ivanvelchev <ivan.velchev.a61@learn.telerikacademy.com>
2024-10-07 15:59:51 +0300

    Atlassian Framework

--- src/test/java/atlassiantests/unums/JiraTestData.java
+++ src/test/java/atlassiantests/unums/JiraTestData.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 public enum JiraTestData {
     username ("ivan.velchev.a61@learn.telerikacademy.com"),
-    password ("**********"),
+    password ("************"),
     jiraProjectName("");
 
     private  String value;
